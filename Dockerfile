8143e193de1e079ad0ac7b897e31f0c92d9e1a89
Marcel Ghostine <mghostine@icloud.com>
2023-05-24 16:38:48 +0200

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:11
+FROM eclipse-temurin:17
 ARG JAR_FILE=build/libs/*.jar
 COPY ${JAR_FILE} app.jar
 EXPOSE 8081
--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,6 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
+//tasks.withType<Test> {
+//	useJUnitPlatform()
+//}
