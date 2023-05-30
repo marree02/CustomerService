1e6cd00fe6d9292f32c3c06fc53e577ddbf96ca4
Marcel Ghostine <mghostine@icloud.com>
2023-05-30 13:42:12 +0200

    fix

--- src/main/java/com/example/customerservice/config/WebSecurityConfig.java
+++ src/main/java/com/example/customerservice/config/WebSecurityConfig.java
@@ -1,20 +1,17 @@
 package com.example.customerservice.config;
 
+import jakarta.servlet.DispatcherType;
 import org.slf4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import jakarta.servlet.DispatcherType;
 
 
 @Configuration
@@ -23,13 +20,6 @@ public class WebSecurityConfig {
 
     private Logger logger = org.slf4j.LoggerFactory.getLogger(WebSecurityConfig.class);
 
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//        System.out.println(passwordEncoder.encode("ThePassword"));
-//        logger.info("The encoded password is: " + passwordEncoder.encode("ThePassword"));
-//        return passwordEncoder;
-//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
@@ -39,9 +29,7 @@ public class WebSecurityConfig {
                         .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
                         .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/customers/**").hasRole("USER")).httpBasic(Customizer.withDefaults());
-//                .formLogin(Customizer.withDefaults());
         http.cors().disable().csrf().disable();
-
         return http.build();
     }
 
--- src/main/java/com/example/customerservice/controller/CustomerController.java
+++ src/main/java/com/example/customerservice/controller/CustomerController.java
@@ -1,18 +1,17 @@
 package com.example.customerservice.controller;
 
-
 import com.example.customerservice.models.Customer;
 import com.example.customerservice.repositories.CustomerRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.validation.annotation.Validated;
-
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@Tag(name = "CustomerController", description = "Endpoints for getting all customers")
+@Tag(name = "CustomerController", description = "Endpoints for managing customers")
 @Validated
 public class CustomerController {
 
@@ -22,16 +21,19 @@ public class CustomerController {
         this.repository = repository;
     }
 
+    @Operation(summary = "Get all customers", description = "Returns a list of all customers")
     @GetMapping("/customers")
     public List<Customer> getCustomers() {
         return this.repository.findAll();
     }
 
+    @Operation(summary = "Get customer by id", description = "Returns a specific customer by their id")
     @GetMapping("/customers/{id}")
     public Customer getCustomerById(@PathVariable Long id) {
         return this.repository.findById(id).orElse(null);
     }
 
+    @Operation(summary = "Create a new customer", description = "Creates a new customer and returns a success message")
     @PostMapping("/customers")
     public String createCustomer(@RequestBody @Valid Customer customer) {
         this.repository.save(customer);
