9e6e19dc7beff9b0d3b3c962d514b10a3026c2cf
Marcel Ghostine <mghostine@icloud.com>
2023-05-30 19:45:31 +0200

    fix

--- src/main/java/com/example/customerservice/models/Customer.java
+++ src/main/java/com/example/customerservice/models/Customer.java
@@ -29,9 +29,31 @@ public class Customer {
     public Customer(String name, String ssn) {
         this.name = name;
         this.ssn = ssn;
+     //   this.shopOrders = shopOrders;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
     }
     
     @Override
